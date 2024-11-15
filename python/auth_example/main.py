e954ef45e7209dfb3366b4f9ef83bc4a89c5ca89
Zhanwu Xiong <488359+zhanwu@users.noreply.github.com>
2024-11-15 11:30:54 -0800

    create a TokenManager class

--- python/auth_example/main.py
+++ python/auth_example/main.py
@@ -1,4 +1,5 @@
 import json
+import urllib.parse
 import logging
 import os
 import time
@@ -14,73 +15,75 @@ logging.basicConfig(
     level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s"
 )
 
-CIPHEROWL_API_URL = "https://svc.cipherowl.ai"
-CLIENT_ID = os.getenv("CLIENT_ID")
-CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-
 
 TokenCache = namedtuple("TokenCache", ["access_token", "expires_at"])
-token_cache = None
 
 
-def get_token_from_cache():
-    if token_cache is None:
-        return None
+class TokenManager:
+    def __init__(self, base_url: str, client_id: str, client_secret: str):
+        self.base_url = urllib.parse.urlparse(base_url)
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.token_cache = None
 
-    if time.time() > token_cache.expires_at:
-        return None
+    def _get_token_from_cache(self):
+        if self.token_cache is None:
+            return None
 
-    logging.debug("Get token from cache")
-    return token_cache.access_token
+        if time.time() > self.token_cache.expires_at:
+            return None
 
+        logging.debug("Get token from cache")
+        return self.token_cache.access_token
 
-def write_token_to_cache(token):
-    global token_cache
-    decoded = jwt.decode(token, options={"verify_signature": False})
-    expires_at = decoded["exp"]
-    token_cache = TokenCache(access_token=token, expires_at=expires_at)
-    logging.debug("Write token to cache")
+    def _write_token_to_cache(self, token):
+        decoded = jwt.decode(token, options={"verify_signature": False})
+        expires_at = decoded["exp"]
+        self.token_cache = TokenCache(access_token=token, expires_at=expires_at)
+        logging.debug("Write token to cache")
 
+    def _get_token_from_server(self):
+        url = urllib.parse.urlunparse(self.base_url._replace(path="/oauth/token"))
 
-def get_token_from_server():
-    url = f"{CIPHEROWL_API_URL}/oauth/token"
+        payload = json.dumps(
+            {
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+                "audience": self.base_url.netloc,
+                "grant_type": "client_credentials",
+            }
+        )
+        headers = {"Content-Type": "application/json"}
 
-    payload = json.dumps(
-        {
-            "client_id": CLIENT_ID,
-            "client_secret": CLIENT_SECRET,
-            "audience": "svc.cipherowl.ai",
-            "grant_type": "client_credentials",
-        }
-    )
-    headers = {"Content-Type": "application/json"}
+        response = requests.request("POST", url, headers=headers, data=payload)
+        response.raise_for_status()
 
-    response = requests.request("POST", url, headers=headers, data=payload)
-    response.raise_for_status()
-
-    token = response.json()["access_token"]
-    logging.debug("Get token from server")
-    write_token_to_cache(token)
-    return token
-
-
-def get_token():
-    # use token cache to improve performance and reduce server load
-    token = get_token_from_cache()
-    if token:
+        token = response.json()["access_token"]
+        logging.debug("Get token from server")
+        self._write_token_to_cache(token)
         return token
 
-    token = get_token_from_server()
-    return token
+    def get_token(self):
+        # use token cache to improve performance and reduce server load
+        token = self._get_token_from_cache()
+        if token:
+            return token
+
+        token = self._get_token_from_server()
+        return token
 
 
 def main():
-    project = "partner"
-    url = f"{CIPHEROWL_API_URL}/api/v1/sanction?project={project}&chain=bitcoin_mainnet&address=12udabs2TkX7NXCSj6KpqXfakjE52ZPLhz"
 
-    token = get_token()
+    cipherowl_api_url = "https://svc.cipherowl.ai"
+    client_id = os.getenv("CLIENT_ID")
+    client_secret = os.getenv("CLIENT_SECRET")
+
+    token_manager = TokenManager(cipherowl_api_url, client_id, client_secret)
+    token = token_manager.get_token()
     headers = {"Authorization": f"Bearer {token}"}
 
+    url = f"{cipherowl_api_url}/api/v1/sanction?chain=bitcoin_mainnet&address=12udabs2TkX7NXCSj6KpqXfakjE52ZPLhz"
     response = requests.request("GET", url, headers=headers)
     response.raise_for_status()
     print(json.dumps(response.json(), indent=4))
